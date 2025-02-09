ccb48bbd9de883fe448ed7b0e4258036de4e3d6a
Rami Maalouf <rami.rami@ucalgary.ca>
2025-02-08 20:47:41 -0700

    Initial Commit

---
+++ MiniOrbit/MiniOrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniOrbitApp.swift
+//  MiniOrbit
+//
+//  Created by Rami Maalouf on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct MiniOrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiniOrbitTests/MiniOrbitTests.swift
@@ -0,0 +1,16 @@
+//
+//  MiniOrbitTests.swift
+//  MiniOrbitTests
+//
+//  Created by Rami Maalouf on 2025-02-08.
+//
+
+import Testing
+
+struct MiniOrbitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
